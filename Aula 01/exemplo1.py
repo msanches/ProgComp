60a651e61b4bce635641e47e6b379f55ba767783
Marco Antonio Sanches <msanc.net@gmail.com>
2021-02-22 11:32:27 -0300

    aula 01

---
+++ Aula 01/exemplo1.py
@@ -0,0 +1,4 @@
+nome = input("Digite o seu nome: ")
+print("Boa noite", nome)
+
+
---
+++ Aula 01/exemplo2.py
@@ -0,0 +1,3 @@
+print("Sejam bem vindos ao mundo da programação")
+resposta = input("Digite o seu nome: ")
+print("Você digitou: ", resposta)
