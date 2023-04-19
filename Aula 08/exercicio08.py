f950eb465ecd0f8b53c7d246f1a74895eb60e578
Marco Antonio Sanches <marcosanches.dev@gmail.com>
2023-04-19 13:45:59 -0300

    Delete exercicio08.py

--- Aula 08/exercicio08.py
+++
@@ -1,9 +0,0 @@
-print('Entrada:')
-t1 = float(input('Digite a temperatura inicial em ºC: '))
-t2 = float(input('Digite a temperatura final em ºC: '))
-dtc = t2 - t1
-dtf = dtc * 1.8
-
-print('\nSaída:')
-print(f'Variação de temperatura em ºC: {dtc:.2f}ºC')
-print(f'Variação de temperatura em ºF: {dtf:.2f}ºF')
