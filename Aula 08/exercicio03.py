6aa27c515b0e712d55bb1410ae84d346f4e6ddf5
Marco Antonio Sanches <marcosanches.dev@gmail.com>
2023-04-19 13:44:42 -0300

    Add files via upload

--- Aula 08/exercicio03.py
+++ Aula 08/exercicio03.py
@@ -1,14 +1,14 @@
 eleitor = 0
 nEleitor = 0
 soma = 0
+n = 10
 print('Entrada:')
-for i in range(10):
+for i in range(n):
     idade = int(input("Digite a idade do aluno: "))
+    soma += idade
     if idade >= 16:
         eleitor = eleitor+1
-    else:
-        nEleitor = nEleitor+1
-        soma= soma+idade
+
 print('\nSaída')
 print("A quantidade de alunos que podem votar é %d"%eleitor)
-print("A média da idade dos alunos não eleitores é %.2f"%(soma/nEleitor))
+print("A média da idade dos alunos é %.2f"%(soma/n))
